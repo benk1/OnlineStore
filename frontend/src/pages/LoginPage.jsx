03e5019e91ead5c476a1c338d1640e5a8b25e7f3
ben <kakengi_bernard@hotmail.com>
2021-12-08 17:33:22 +0200

    add profile in front-end

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import {
+	Form,
+	Button,
+	Row,
+	Col,
+	FormGroup,
+	FormLabel,
+	FormControl,
+} from 'react-bootstrap';
+import FormContainer from '../components/FormContainer';
+import { useDispatch, useSelector } from 'react-redux';
+import { login } from '../actions/userActions';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+
+const LoginPage = ({ location, history }) => {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+	const dispatch = useDispatch();
+
+	const userLogin = useSelector((state) => state.userLogin);
+	const { loading, error, userInfo } = userLogin;
+
+	const redirect = location.search ? location.search.split('=')[1] : '/';
+
+	useEffect(() => {
+		if (userInfo) {
+			history.push(redirect);
+		}
+	}, [history, userInfo, redirect]);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		dispatch(login(email, password));
+	};
+	return (
+		<FormContainer>
+			<h1>Sign In</h1>
+			{error && <Message variant="danger">{error}</Message>}
+			{loading && <Loader />}
+			<Form onSubmit={handleSubmit}>
+				<FormGroup controlId="email">
+					<FormLabel>Email Address</FormLabel>
+					<FormControl
+						type="email"
+						placeholder="Enter email"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					></FormControl>
+				</FormGroup>
+
+				<FormGroup controlId="password">
+					<FormLabel>Password</FormLabel>
+					<FormControl
+						type="password"
+						placeholder="Enter password"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					></FormControl>
+				</FormGroup>
+				<br />
+				<Button type="submit" variant="primary">
+					Sign In
+				</Button>
+			</Form>
+
+			<Row className="py-3">
+				<Col>
+					New Customer ?{' '}
+					<Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>
+						Register
+					</Link>
+				</Col>
+			</Row>
+		</FormContainer>
+	);
+};
+
+export default LoginPage;
---
+++ frontend/src/pages/RegisterPage.jsx
@@ -0,0 +1,111 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import {
+	Form,
+	Button,
+	Row,
+	Col,
+	FormGroup,
+	FormLabel,
+	FormControl,
+} from 'react-bootstrap';
+import FormContainer from '../components/FormContainer';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, register } from '../actions/userActions';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+
+const RegisterPage = ({ location, history }) => {
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [confirmPassword, setConfirmPassword] = useState('');
+	const [message, setMessage] = useState(null);
+
+	const dispatch = useDispatch();
+
+	const userRegister = useSelector((state) => state.userRegister);
+	const { loading, error, userInfo } = userRegister;
+
+	const redirect = location.search ? location.search.split('=')[1] : '/';
+
+	useEffect(() => {
+		if (userInfo) {
+			history.push(redirect);
+		}
+	}, [history, userInfo, redirect]);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (password !== confirmPassword) {
+			setMessage('Password do not match');
+		} else {
+			dispatch(register(name, email, password));
+		}
+	};
+	return (
+		<FormContainer>
+			<h1>Sign Up</h1>
+			{message && <Message variant="danger">{message}</Message>}
+			{error && <Message variant="danger">{error}</Message>}
+			{loading && <Loader />}
+			<Form onSubmit={handleSubmit}>
+				<FormGroup controlId="name">
+					<FormLabel>Name</FormLabel>
+					<FormControl
+						type="text"
+						placeholder="Enter name"
+						value={name}
+						onChange={(e) => setName(e.target.value)}
+					></FormControl>
+				</FormGroup>
+
+				<FormGroup controlId="email">
+					<FormLabel>Email Address</FormLabel>
+					<FormControl
+						type="email"
+						placeholder="Enter email"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					></FormControl>
+				</FormGroup>
+
+				<FormGroup controlId="password">
+					<FormLabel>Password</FormLabel>
+					<FormControl
+						type="password"
+						placeholder="Enter password"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					></FormControl>
+				</FormGroup>
+
+				<FormGroup controlId="confirmPassword">
+					<FormLabel>Confirm Password</FormLabel>
+					<FormControl
+						type="password"
+						placeholder="Confirm password"
+						value={confirmPassword}
+						onChange={(e) => setConfirmPassword(e.target.value)}
+					></FormControl>
+				</FormGroup>
+				<br />
+				<Button type="submit" variant="primary">
+					Register
+				</Button>
+			</Form>
+
+			<Row className="py-3">
+				<Col>
+					Have an Account ?{' '}
+					<Link to={redirect ? `/login?redirect=${redirect}` : '/login'}>
+						Login
+					</Link>
+				</Col>
+			</Row>
+		</FormContainer>
+	);
+};
+
+export default RegisterPage;
